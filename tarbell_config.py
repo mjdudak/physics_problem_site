4bff6711320ae83df09662ad6d4c60b342c3e32d
Matthew Dudak <compassman495@gmail.com>
2020-08-24 13:30:12 -0500

    Made most of the site

--- tarbell_config.py
+++ tarbell_config.py
@@ -1,3 +1,10 @@
+from flask import Blueprint, render_template, g
+from tarbell.hooks import register_hook
+
+# create a blueprint for this project
+# tarbell will consume this when the project loads
+blueprint = Blueprint('myproject', __name__)
+
 # -*- coding: utf-8 -*-
 
 """
@@ -43,3 +50,24 @@ DEFAULT_CONTEXT = {
              'Problem '
              'Site'
 }
+
+@blueprint.route("/units/<id>.html")
+def unit_page(id):
+    site = g.current_site
+    topics = site.get_context()["unit" + id + "_topics"]
+    units = site.get_context()["units"]
+    return render_template("_unit_page.html", id=id, topics=topics, units=units)
+
+@blueprint.route("/units/<id>/<topic_id>.html")
+def topic_page(id, topic_id):
+    site = g.current_site
+    units = site.get_context()["units"]
+    problems = site.get_context()["unit" + id]
+    topics = site.get_context()["unit" + id + "_topics"]
+    topic = [t for t in topics if t["topic_id"] == int(topic_id)][0]
+    problems_relevant = []
+    for p in problems:
+        if p["id"]:
+            if p["problem_topic"] == topic["topic"] and p["problem_level"] == topic["topic_level"]:
+                problems_relevant.append(p)
+    return render_template("_topic_page.html", id=id, topic=topic, problems=problems_relevant, units=units)
